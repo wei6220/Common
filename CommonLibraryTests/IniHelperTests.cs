bc16e82a76e9e5e43af7d738dfc5de81b1006ba3
wei6220 <wei6220@gmail.com>
2016-08-04 14:22:22 +0800

    add debug

--- CommonLibraryTests/IniHelperTests.cs
+++ CommonLibraryTests/IniHelperTests.cs
@@ -20,7 +20,7 @@ namespace CommonLibrary.Tests
             //Act : 呼叫目標物件的方法
 
             //Assert : 驗證是否符合預期
-            Assert.IsNotNull(actual);
+            Assert.IsNotNull(actual, "Jerry Debug :" + actual);
             //Assert.AreEqual("OK", actual);
         }
 
